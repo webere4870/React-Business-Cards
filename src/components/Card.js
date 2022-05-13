a1cb084761eb373341796e63507906a5ce538390
Eli Weber <eliweber2001@gmail.com>
2022-05-12 21:28:57 -0400

    Initial React commit

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Picture from './Picture'
+import Main from './Main'
+import Footer from './Footer'
+function Card()
+{
+    return (
+        <div className="underCard colFlex">
+            <Picture/>
+        </div>
+        
+    )
+}
+
+export default Card
---
+++ src/components/Picture.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Profile from './../images/bauer.jpg'
+function Picture()
+{
+    return(
+        <img src={Profile} alt="" />
+    )
+}
+
+export default Picture
